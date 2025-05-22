664705c6c034a0fbae2dfc195d4278dc2905dd4c
JayShippey99 <89869985+JayShippey99@users.noreply.github.com>
2025-05-22 17:08:38 -0400

    Update data.js
    added period

--- assets/js/data.js
+++ assets/js/data.js
@@ -15,7 +15,7 @@ xhttp.onreadystatechange = function () {
       
     // Portfolio data
     dataPortfolio.forEach((data, index) => {
-        // let portfolioGoalsList = dataPortfolio["goals_list"]; // this might not get the correct goals list
+        // let portfolioGoalsList = dataPortfolio["goals_list"]; // this might not get the correct goals list .
         
       openModalPortfolio[index].addEventListener("click", () => {
         modalWrapper.innerHTML = `
